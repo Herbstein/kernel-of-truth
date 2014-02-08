8b08bdbbb66eab758142ba94188b648f69ef3977
Ian Kronquist <iankronquist@teleport.com>
2014-02-07 18:29:26 -0800

    Remove register struct from C source.

--- kernel/isr.h
+++ kernel/isr.h
@@ -1,6 +1,6 @@
 #ifndef ISR_H
 #define ISR_H
-
+#include "registers.h"
 #include "kputs.h"
 #include "kabort.h"
 #include "idt.h"
@@ -42,14 +42,6 @@ void isrs_install();
 
 
 
-/* This defines what the stack looks like after an ISR was running */
-struct regs
-{
-    unsigned int gs, fs, es, ds;      /* pushed the segs last */
-    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;  /* pushed by 'pusha' */
-    unsigned int int_no, err_code;    /* our 'push byte #' and ecodes do this */
-    unsigned int eip, cs, eflags, useresp, ss;   /* pushed by the processor automatically */ 
-};
 
 
 void fault_handler(struct regs *r);

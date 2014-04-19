4eadbf6ea10472e44f983f882c799a9c856e8b4d
Ian Kronquist <iankronquist@teleport.com>
2014-04-18 22:49:48 -0700

    gdt.c: Remove bad assert. Add comments

--- kernel/arch/i586/gdt.c
+++ kernel/arch/i586/gdt.c
@@ -2,14 +2,12 @@
 
 GDT_t create_gdt_record(uint32_t base, uint32_t limit, uint8_t access_byte, uint8_t flags)
 {
-    // The top byte of the access byte should always be 1
-    kassert(access_byte == 0xff);
     GDT_t gdt_record = 0;
     GDT_t buffer;
     gdt_record |= limit                & 0x000000000000ffff; 
     gdt_record |= (base << 16)         & 0x000000ffffff0000; 
     buffer = access_byte;
-    gdt_record |= (buffer << 41)  & 0x0000ff0000000000;
+    gdt_record |= (buffer << 41)       & 0x0000ff0000000000;
     buffer = limit;
     gdt_record |= (buffer << (49-16))  & 0x000f000000000000;
     buffer = flags;
@@ -25,10 +23,10 @@ void set_up_gdt()
     GDT_t gdt[3];
     // Null descriptor - will never be used by processor
     gdt[0] = create_gdt_record(0,0,0,0);
-    // Code segment
-    gdt[1] = create_gdt_record(0, 0xffffffff, 0xff, 7); 
-    // Data segment
-    gdt[2] = create_gdt_record(0, 0xffffffff, 0xff, 7);
+    // Code segment, theoretically, really the flags are set to allow anything
+    gdt[1] = create_gdt_record(0, 0xffffffff, 0xff, 0xff); 
+    // Data segment, theoretically, really the flags are set to allow anything
+    gdt[2] = create_gdt_record(0, 0xffffffff, 0xff, 0xff);
     gdt_ptr->table_start = gdt;
     gdt_ptr->table_size = 3 * 64 - 1;
     enter_protected_mode();
@@ -37,10 +35,12 @@ void set_up_gdt()
 void enter_protected_mode()
 {
     __asm__ __volatile__ (
-    "cli\n" //disable interrupts
-    "lgdt gdt_ptr\n" //load GDT register with start address of GDT"
-    "mov %eax, %cr0\n"
-    "or %al, 1\n" //set PE (Protection Enable) bit in CR0 (Control Register 0)"
-    "mov %cr0, %eax\n"
+    "cli\n"            // disable interrupts
+    "lgdt gdt_ptr\n"   // load GDT register with start address of GDT
+    "mov %cr0, %eax\n" // move the contents of the control register 0 to the
+                       // eax register
+    "or %al, 1\n"      // set PE (Protection Enable) bit in CR0 
+                       // (Control Register 0)
+    "mov %eax, %cr0\n" // Put the contents of eax back into the eax register
     );
 }
